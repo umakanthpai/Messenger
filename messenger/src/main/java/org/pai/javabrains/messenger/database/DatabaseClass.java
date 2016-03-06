e564255d819aa7503a1e130360f7866605d40fd6
Umakanth Pai <umakanth.pai@outlook.com>
2016-03-06 20:26:11 +0100

    JAXRS return message in XML format

---
+++ messenger/src/main/java/org/pai/javabrains/messenger/database/DatabaseClass.java
@@ -0,0 +1,26 @@
+package org.pai.javabrains.messenger.database;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.pai.javabrains.messenger.model.Message;
+import org.pai.javabrains.messenger.model.Profile;
+
+public class DatabaseClass {
+
+	private static Map<Long, Message> messages = new HashMap<>();
+	private static Map<String, Profile> profiles = new HashMap<>();
+
+	
+	public static Map<Long, Message> getMessages() {
+		return messages;
+	}
+	
+	public static Map<String, Profile> getProfiles() {
+		return profiles;
+	}
+
+	
+	
+	
+}
---
+++ messenger/src/main/java/org/pai/javabrains/messenger/resources/MessageResource.java
@@ -0,0 +1,21 @@
+package org.pai.javabrains.messenger.resources;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.pai.javabrains.messenger.model.Message;
+import org.pai.javabrains.messenger.service.MessageService;
+
+@Path("/messages")
+public class MessageResource {
+	MessageService messageService = new MessageService();
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	public List<Message> getMessages(){
+		return messageService.getAllMessages();
+	}
+}
