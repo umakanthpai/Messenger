abfa4c94ffab3accf12268921e34b95a1fd6b790
Umakanth Pai <umakanth.pai@outlook.com>
2016-03-13 23:36:54 +0100

    Path params

--- messenger/src/main/java/org/pai/javabrains/messenger/resources/MessageResource.java
+++ messenger/src/main/java/org/pai/javabrains/messenger/resources/MessageResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -17,5 +18,12 @@ public class MessageResource {
 	@Produces(MediaType.APPLICATION_XML)
 	public List<Message> getMessages(){
 		return messageService.getAllMessages();
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	@Path("/{msgID}")
+	public Message getMessage(@PathParam("msgID") long msgID){
+		return messageService.getMessage(msgID);
 	}
 }
